226d62294e3d947a278c9cf93aebfa705802ac0a
coleworsley <cole.s.worsley@gmail.com>
2017-08-24 19:46:05 -0600

    pass initial tests

--- elevator.js
+++ elevator.js
@@ -1,5 +1,21 @@
 export default class Elevator {
   constructor() {
-    
+    currentFloor: 0;
+    requests: {};
+    riders: {};
+  }
+
+  goToFloor(user) {
+    while(this.currentFloor !== user.dropOffFloor) {
+      if (this.currentFloor < user.dropOffFloor) {
+        this.currentFloor++
+      } else {
+        this.currentFloor--
+      }
+    }
+  }
+
+  reset() {
+    this.currentFloor = 0;
   }
 }
